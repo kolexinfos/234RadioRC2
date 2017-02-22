fee16eae203a3cec747ec11a437916ab968bb814
Kehinde Kolade <kolexinfos@gmail.com>
2017-02-22 10:48:13 +0100

    Added Upload file functionality

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController, LoadingController } from 'ionic-angular';
 
-import { Toast, ImagePicker } from 'ionic-native';
+import { Toast, ImagePicker, Transfer } from 'ionic-native';
 
 import { HomePage } from '../home/home';
 
@@ -17,7 +17,9 @@ import {Plugins} from '../../providers/plugin.service';
 export class ReportPage {
 
 
-    report: {email?: string, message?: string, phone?: string,title?: string,type?:string} = {};
+    report: {email?: string, message?: string, phone?: string,title?: string,type?:string, fileUrl?:any} = {};
+
+    uri:string = '';
 
     submit = false;
 
@@ -31,12 +33,14 @@ export class ReportPage {
     FileSelected(){
       let options = {
         // quality of resized image, defaults to 100
-        quality: 99
+        quality: 99,
+        maximumImagesCount:1
       };
 
       ImagePicker.getPictures(options).then((results) => {
         for (var i = 0; i < results.length; i++) {
           console.log('Image URI: ' + results[i]);
+          this.uri = results[i]
         }
       }, (err) => { });
     }
@@ -45,10 +49,11 @@ export class ReportPage {
     {
         console.log("The details in the form is " + form);
         this.submit = true;
-
+        this.upload();
+        
         this.report.type = "report";
         if(form.valid){
-
+            
             let loadingPopup = this.loadingCtrl.create({
                   content: 'Loading data...'
             });
@@ -86,6 +91,25 @@ export class ReportPage {
                 )
         }
     }
+
+    upload(){
+        const fileTransfer = new Transfer();
+        var options: any;
+        var filename = this.report.email + new Date().getUTCMilliseconds();
+
+        options = {
+            fileKey: 'file',
+            fileName: filename,
+            headers: {}
+        }
+        fileTransfer.upload(this.uri, "https://shoppa.herokuapp.com/users/", options)
+        .then((data) => {
+            console.log(data)
+        }, (err) => {
+            // error
+            console.log(err);
+        })
+}
 
 
 }
